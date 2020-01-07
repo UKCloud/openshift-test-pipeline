e9cf71b168766bc83f2741a3686c8326c0d43546
dbrennand <dbrennand@ukcloud.com>
2020-01-07 15:54:09 +0000

    Add run-pipeline.sh

---
+++ run-pipeline.sh
@@ -0,0 +1,18 @@
+read -p "Provide generic webhook URL (from above): " WEBHOOK_URL
+
+# Obtain OpenShift secret to pass in through webhook.
+SECRET=$(oc get secret openshift -o json)
+
+# Trigger pipeline build via webhook.
+# Provide environment variables.
+ENV_VARS='env:
+- name: "RemoteTrigger"
+    value: "False"
+- name: "Credentials"
+    value: ${SECRET}'
+
+# Find '<secret>'' in url and replace with $BASE64STRING.
+WEBHOOK_URL="${WEBHOOK_URL/<secret>/$BASE64STRING}"
+
+# Send request to kick off pipeline using 
+curl -H "Content-Type: application/yaml" -d $ENV_VARS -X POST $WEBHOOK_URL
