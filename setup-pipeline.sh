7c748acc2d2b101b60e8e8ab4999ef31902a99cd
dbrennand <dbrennand@ukcloud.com>
2020-01-07 11:39:19 +0000

    Add dev setup script.

---
+++ setup-pipeline.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Declare variables.
+NAME="openshift-test-pipeline-slave"
+SOURCE_REPOSITORY_URL="https://github.com/UKCloud/openshift-test-pipeline.git"
+SOURCE_REPOSITORY_REF="dev"
+CONTEXT_DIR="docker"
+PROJECT="openshift-test-pipeline"
+
+# Setup pipeline project in OpenShift.
+oc new-project $PROJECT
+
+# Switch OpenShift project.
+oc project $PROJECT
+
+# Deploy jenkins master.
+oc new-app jenkins-persistent -p VOLUME_CAPACITY=50Gi
+
+# Deploy jenkins slave.
+oc new-app -f jenkins-pipelines/openshift-test-slave.yaml \
+    -p NAME=$NAME \
+    -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL \
+    -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
+    -p CONTEXT_DIR=$CONTEXT_DIR
+
+# Create Base64 string to use as pipeline generic webhook secret.
+BASE64STRING=$(echo \$RANDOM | base64)
+# Deploy buildconfig for test pipeline. Creates generic webhook.
+oc create -f jenkins-pipelines/test-pipeline.yaml -p SECRET=$BASE64STRING
+
+# Once created, describe object telling the user how to kick off a pipeline.
+oc describe bc/openshift-test-pipeline
+echo "Use the Webhook Generic URL above to kick off the pipeline."
