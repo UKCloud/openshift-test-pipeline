1d3ba6b46b4b0831f46a8d7a505515162559f43c
dbrennand <dbrennand@ukcloud.com>
2020-01-08 13:09:14 +0000

    Fixed ENV_VARS variable.

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -7,12 +7,12 @@ SECRET=$(oc get secret openshift -o json)
 
 # Trigger pipeline build via webhook.
 # Provide environment variables.
-ENV_VARS='env:
-- name: "Credentials"
-  value: ${SECRET}'
+ENV_VARS="env:
+- name: 'Credentials'
+  value: $SECRET"
 
 # Find '<secret>'' in url and replace with $BASE64STRING.
 WEBHOOK_URL="${WEBHOOK_URL/<secret>/$BASE64STRING}"
 
 # Send request to kick off pipeline using 
-curl -H "Content-Type: application/yaml" -d $ENV_VARS -X POST $WEBHOOK_URL
+curl -H "Content-Type: application/yaml" -d $ENV_VARS -X "POST" $WEBHOOK_URL
